7a8b085b68ffbd72951d5ba2b389815709cf7205
codelta-ops <2014814075@qq.com>
2025-12-21 22:25:49 +0800

    Update WebMvcConfiguration.java

--- give_hand/give_hand/give_hand-server/src/main/java/com/give_hand/config/WebMvcConfiguration.java
+++ give_hand/give_hand/give_hand-server/src/main/java/com/give_hand/config/WebMvcConfiguration.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -85,5 +86,20 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         // swagger-ui
         registry.addResourceHandler("/swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
+    }
+
+    /**
+     * CORS 配置
+     */
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://my-friends-5.pages.dev"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
