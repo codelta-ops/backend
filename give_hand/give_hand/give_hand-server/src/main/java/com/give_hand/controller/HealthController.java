9394eabc1f0f2a6fc27c68efd325be8b6b033c61
codelta-ops <2014814075@qq.com>
2025-12-21 14:11:36 +0800

    1

---
+++ give_hand/give_hand/give_hand-server/src/main/java/com/give_hand/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.give_hand.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+    @GetMapping("/healthz")
+    public String healthz() {
+        return "OK";
+    }
+}
