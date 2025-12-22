3c91bd8bea1ef32dbb7fa88c863b4ef4f2a3e0e1
codelta-ops <2014814075@qq.com>
2025-12-22 10:01:26 +0800

    Update JwtTokenUserInterceptor.java

--- give_hand/give_hand/give_hand-server/src/main/java/com/give_hand/interceptor/JwtTokenUserInterceptor.java
+++ give_hand/give_hand/give_hand-server/src/main/java/com/give_hand/interceptor/JwtTokenUserInterceptor.java
@@ -21,6 +21,10 @@ public class JwtTokenUserInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 放行预检请求，避免 CORS 失败
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
 
         String tokenName = jwtProperties.getUserTokenName(); // 一般是 token
         String token = request.getHeader(tokenName);
